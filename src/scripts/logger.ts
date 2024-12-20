3eeb803b28283b3a83c703cd5d0b7315040c4990
Alibi Duisenaliyev <108901007+realibi-dev@users.noreply.github.com>
2024-12-20 14:45:33 +0500

    debug

--- src/scripts/logger.ts
+++ src/scripts/logger.ts
@@ -1,5 +1,5 @@
 // import fs from "fs";
-import moment from "moment";
+// import moment from "moment";
 const LOG_FILE_PATH = process.env.NEXT_PUBLIC_LOG_FILE_PATH || "./logs.txt";
 
 export const enum MESSAGE_TYPES {
@@ -8,6 +8,6 @@ export const enum MESSAGE_TYPES {
 }
 
 export const writeLog = (type: MESSAGE_TYPES, message: string) : void => {
-	const text = `[${moment().format("DD/MM/YYYY HH:mm:ss")}] ${type}: ${message} \n`;
+	// const text = `[${moment().format("DD/MM/YYYY HH:mm:ss")}] ${type}: ${message} \n`;
 	// fs.appendFileSync(LOG_FILE_PATH, text);
 }
